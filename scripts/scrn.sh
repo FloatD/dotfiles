6a46fd3010974150cbbe49aa7d088a22cf3e5689
FloatD <20p.leo10@gmail.com>
2016-03-14 19:47:40 +0200

    Smarter

--- scripts/scrn.sh
+++ scripts/scrn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR='/home/intnull/Pictures/scrot'
+DIR="/home/${USER}/Pictures/scrot"
 DATE_DIR=$(date +%Y)
 
 
@@ -8,10 +8,19 @@ DATE_FILE=$(date +%m.%d)
 TIME_FILE=$(date +%H:%M)
 FFILE="${DATE_FILE}_${TIME_FILE}_scrot.png"
 
-screenfetch
+if [ ! -d "$DIR" ]; then
+	mkdir $DIR
+fi
 if [ ! -d "${DIR}/${DATE_DIR}" ]; then
 	mkdir "${DIR}/${DATE_DIR}"
 fi
+screenfetch
+echo "Wait 3... "
 sleep 1
+echo "2... "
+sleep 1
+echo "1... "
+sleep 1
+echo "0... "
 scrot "$FFILE"
 mv "$FFILE" "${DIR}/${DATE_DIR}"
